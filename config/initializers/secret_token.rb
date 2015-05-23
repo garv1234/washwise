6c0a760c9457ec56a2f972cf78ea5f6ca66204b6
garv123 <garv.mathur@gmail.com>
2015-05-24 03:04:40 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Washwise::Application.config.secret_token = '827140382d24068c7ef132b855623d8e4d276a7a66cfd3d45c8c1f30dcbd3998ed4966b0a577cb57258ba081ff1ac762670da4ae70bc4f85171fcacd7455c602'
